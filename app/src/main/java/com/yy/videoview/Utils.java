d28bec51ce34d9db0cda2c11f89fa954560b60c9
yangyang <13160677911@163.com>
2017-03-18 17:48:48 +0800

    3月3日半夜更新

--- app/src/main/java/com/yy/videoview/Utils.java
+++ app/src/main/java/com/yy/videoview/Utils.java
@@ -5,6 +5,6 @@ package com.yy.videoview;
  */
 
 public class Utils {
-    public static final String videoUrl = "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4" ;
+    public static final String videoUrl = "http://ojnecqsh3.bkt.clouddn.com/20170126Zengyong-rangyasuiqianjinshangtianhua-01.mp4" ;
     public static final String video_type = "VIDEO_TYPE";
 }
